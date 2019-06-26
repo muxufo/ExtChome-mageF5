a16ad78781214696d5af92760e3a249735e5465b
Nicolas Crelier <crelier.nicolas@gmail.com>
2019-06-26 04:18:35 -0400

    Added input box + constant refresh + input has a number for user (scree)

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 function getNowDate() {
     var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     var time = today.getHours() + '-' + today.getMinutes() + '-' + today.getSeconds();
     return date + '--' + time;
 }
@@ -11,7 +11,7 @@ var screenshot = {
 
     init: function () {
         console.log('init SCREE');
-        this.initEvents();
+        this.takeScreenshot();
     },
     saveScreenshot: function () {
         var image = new Image();
@@ -27,37 +27,32 @@ var screenshot = {
 
             var fileName = getNowDate();
 
-            link.download =  fileName +".png";
+            link.download = fileName + ".png";
             link.href = screenshot.content.toDataURL();
             link.click();
             screenshot.data = '';
         };
         image.src = screenshot.data;
     },
-    initEvents: function () {
-        console.log('init event addlistener screen');
-        chrome.runtime.onMessage.addListener(
-            function (request, sender, sendResponse) {
+    takeScreenshot: function () {
+        chrome.tabs.captureVisibleTab(null, {format: "png"}, function (data) {
+            screenshot.data = data;
+            screenshot.saveScreenshot();
 
-                console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");
-                if (request.message === "go") {
+        });
 
-                    chrome.tabs.captureVisibleTab(null, {format: "png"}, function (data) {
-                        screenshot.data = data;
-                        screenshot.saveScreenshot();
 
-                    });
-                    sendResponse({message: "success"});
-
-                }
-                return true;
-            });
     }
 };
 
+var countdown = {
+    remain: 0,
+};
 
+chrome.runtime.onConnect.addListener(function (port) {
+    console.assert(port.name === "letmeiiiiin");
+    port.postMessage({textValue: countdown.remain});
+});
 
 // chrome extension keepAlive
 
@@ -87,11 +82,6 @@ function my_listener(tabId, changeInfo, tab) {
     // If updated tab matches this one
     if (changeInfo.status == "complete" && tabId == the_tab_id && status == 'on') {
         toggle_extension(tab);
-        screenshot.init();
-
-        // chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        //     chrome.tabs.reload(tabs[0].id, {bypassCache: true})
-        // });
     }
 }
 
@@ -100,35 +90,67 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     console.log('set_status');
     toggle_extension(tab);
     console.log('toggle_extension');
-    screenshot.init();
-    console.log('init screenshots');
-
 });
 
-function reloadPage(){
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.message === "init") {
+            countdown.remain = request.coutndown;
+            sendResponse({message: "success"});
+        }
+    }
+);
+
+
+
+function reloadPage() {
     console.log('Reloading page...');
-    chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+    console.log('Remaining screenshot: ' + countdown.remain);
+    if (countdown.remain > 0) {
+        screenshot.init();
+        countdown.remain--;
+    }
+
+
+    // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    //     if (changeInfo.status === 'complete') {
+    //         chrome.tabs.query({ active: true }, function(tabs) {
+    //             const msg = "Hello from background";
+    //             chrome.tabs.sendMessage(tabs[0].id, { "message": msg });
+    //         })
+    //     }
+    // });
+
+    chrome.tabs.query({active: true}, function (arrayOfTabs) {
+
+
         console.log('in the query');
         console.log('---------------');
+        var time = Date.now();
+        chrome.browsingData.removeCache({"since": time}, function () {
+        });
         chrome.tabs.reload(arrayOfTabs[0].id, {bypassCache: true});
+
     });
 }
 
 
-chrome.tabs.onUpdated.addListener(my_listener);
-// screenshot.init();
-console.log('listener added');
-//
-// setInterval(function() {
-//     reloadPage()
-// }, 10000);
 
+
+
+setInterval(function () {
+    reloadPage()
+}, 5000);
 
 /*
-1 sec = 1000ms
-10 sec = 10000ms
-100 sec = 100000ms
-900 sec = 900000ms
+1 sec = 1000 ms
+10 sec = 10000 ms
+100 sec = 100000 ms
+900 sec = 900000 ms
  */
+
 
 
--- content.js
+++ content.js
@@ -1,5 +1,28 @@
-chrome.runtime.sendMessage({message: "go"}, function(response) {
-    console.log('Response is: ' + response.message );
+$('#start-button').on('click', function () {
+    var valueText = $('#text-time').val();
+
+    chrome.runtime.sendMessage({message: "init", coutndown: valueText}, function (response) {
+
+    });
+});
+
+var port = chrome.runtime.connect({name: "letmeiiiiin"});
+port.onMessage.addListener(function (msg) {
+    console.log(msg);
+    var valueText = msg.textValue;
+    console.log('valueText: ' + valueText);
+    $('#text-time').val(valueText);
+
 });
 
 
+//
+// chrome.runtime.onMessage.addListener(
+//     function(message, sender, sendResponse) {
+// console.log(message);
+//         if (message.update === "updateNumber"){
+//             sendResponse({message: "success"});
+//
+//         }
+//
+//     });
